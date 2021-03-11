f9cc8321352467e5473ec777d8ceddedf0a2d47a
remi <info@remialkemade.nl>
2021-03-11 17:21:59 +0000

    Removed old dist files

--- dist/ModelFactoryInterface.d.ts
+++
@@ -1,6 +0,0 @@
-import Model, { ModelConstructor, ModelData } from "@/Model";
-export default interface ModelFactoryInterface {
-    create<T extends Model>(ExpectedClass: ModelConstructor<T>, data?: ModelData<T>, root?: boolean, asReference?: boolean): T;
-    destroy(model: Model): void;
-}
-export declare const ModelFactoryType: unique symbol;
--- dist/ModelFactoryInterface.js
+++
@@ -1,5 +0,0 @@
-/*
-Simple definition file to prevent circular dependencies between ModelFactory and Model
- */
-export const ModelFactoryType = Symbol.for("ModelFactory");
-//# sourceMappingURL=ModelFactoryInterface.js.map

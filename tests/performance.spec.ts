1d24a693ee70f92b2c7a17567828d90c4092e995
Remi Alkemade <remi.alkemade@demcon.com>
2024-07-08 14:30:16 +0200

    Improved performance (sharpened test criteria)

--- tests/performance.spec.ts
+++ tests/performance.spec.ts
@@ -14,7 +14,7 @@ function time(nTimes:number, callback:(iteration:number)=>void) {
 describe("Performance", () => {
 	describe("Mozel", () => {
 		describe("setting a property", () => {
-			it("without any watchers takes less than 100 times as long as setting a property on a plain object", () => {
+			it("without any watchers takes less than 70 times as long as setting a property on a plain object", () => {
 				class Foo extends Mozel {
 					@property(String)
 					foo?:string;
@@ -26,9 +26,9 @@ describe("Performance", () => {
 				const duration = time(nTimes, i => foo.foo = i.toString());
 				const refDuration = time(nTimes, i => ref.foo = i.toString());
 
-				assert.isBelow(duration, refDuration * 100);
+				assert.isBelow(duration, refDuration * 50);
 			});
-			it("with a watcher takes less than 25 times as long as setting a property without a watcher", () => {
+			it("with a watcher takes less than 3 times as long as setting a property without a watcher", () => {
 				class Foo extends Mozel {
 					@property(String)
 					foo?:string;
@@ -42,9 +42,9 @@ describe("Performance", () => {
 				const duration = time(nTimes, i => foo.foo = i.toString());
 				const refDuration = time(nTimes, i => ref.foo = i.toString());
 
-				assert.isBelow(duration, refDuration * 25);
+				assert.isBelow(duration, refDuration * 3);
 			});
-			it("with 2 watchers takes less than 4 times as long as setting a property with 1 watcher", () => {
+			it("with 2 watchers takes less than 2 times as long as setting a property with 1 watcher", () => {
 				class Foo extends Mozel {
 					@property(String)
 					foo?:string;
@@ -59,7 +59,7 @@ describe("Performance", () => {
 				const duration = time(100000, i => foo.foo = i.toString());
 				const refDuration = time(100000, i => ref.foo = i.toString());
 
-				assert.isBelow(duration, refDuration * 4);
+				assert.isBelow(duration, refDuration * 2);
 			});
 		});
 		describe("traversing a Collection", () => {
